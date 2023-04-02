b0d1d3bd6dd8d0e59357245daf370c62b6e5f96c
Jung1er <62351396+Jung1er@users.noreply.github.com>
2023-04-02 23:10:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,6 @@ chat_id = 392609262
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    return 0.5 - expon.ppf(1 - alpha / 2) / (len(x) * min(x) / 53**2), 0.5 - expon.ppf(alpha / 2) / (len(x) * min(x) / 53**2)
+    a = expon.ppf(alpha / 2) / (len(x) * min(x - x.mean())) + x.mean()
+    b = expon.ppf(1 - alpha / 2) / (len(x) * min(x - x.mean())) + x.mean()
+    return min(a,b) / 53**2, max(a,b) / 53**2
