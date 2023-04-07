b0c8178a5df28ee7980777f4eea36b137c3c27f0
Jung1er <62351396+Jung1er@users.noreply.github.com>
2023-04-07 13:44:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import expon
+from scipy.stats import gamma
 
 
 chat_id = 392609262
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    errors = x - x.mean()
-    a_errors_exp = expon.ppf(alpha / 2) / (len(x) * min(errors))
-    b_errors_exp = expon.ppf(1 - alpha / 2) / (len(x) * min(errors))
-    a_errors_final = 0.5 - b_errors_exp
-    b_errors_final = 0.5 - a_errors_exp
-    return (min(a_errors_exp, b_errors_final) + x.mean()) / 53**2, (max(a_errors_exp, b_errors_final) + x.mean()) / 53**2
+    n = len(x)
+    
+    left_gamma = gamma.ppf(q = alpha / 2, a = n, scale = 1 / n)
+    right_gamma = gamma.ppf(q = 1 - alpha / 2, a = n, scale = 1 / n)
+    
+    left = 2 * (left_gamma + x.mean() - 0.5) / (53 ** 2)
+    right = 2 * (right_gamma + x.mean() - 0.5) / (53 ** 2)
+    
+    return (left, right)
